69dbd138df93d311bc3f474d45579a808aac8df2
skyjet18 <57456827+skyjet18@users.noreply.github.com>
2024-03-22 13:59:21 +0100

    fixed setting info flags
    this fixes h265 playback of non-TS file

--- container/container_ffmpeg.c
+++ container/container_ffmpeg.c
@@ -917,6 +917,7 @@ static void FFMPEGThread(Context_t *context)
                     avOut.width      = videoTrack->width;
                     avOut.height     = videoTrack->height;
                     avOut.type       = "video";
+                    avOut.infoFlags  = 0;
                     
                     if (avContextTab[cAVIdx]->iformat->flags & AVFMT_TS_DISCONT)
                     {
