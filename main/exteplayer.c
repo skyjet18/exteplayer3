76b92e91fa9d48de38ef6bda807b503ec91d1886
skyjet18 <57456827+skyjet18@users.noreply.github.com>
2024-05-21 09:36:25 +0200

    updated version to 181

--- main/exteplayer.c
+++ main/exteplayer.c
@@ -755,7 +755,7 @@ int main(int argc, char* argv[])
     int commandRetVal = -1;
 
     /* inform client that we can handle additional commands */
-    E2iSendMsg("{\"EPLAYER3_EXTENDED\":{\"version\":%d}}\n", 68);
+    E2iSendMsg("{\"EPLAYER3_EXTENDED\":{\"version\":%d}}\n", 181);
 
     PlayFiles_t playbackFiles;
     memset(&playbackFiles, 0x00, sizeof(playbackFiles));
